a909a1d3dafff8bc7205cb2707d8e84adc9b9b1f
Dan DeFelippi <dan@driverdan.com>
2010-08-30 17:28:23 -0400

    Live updates working

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,15 @@ $(function() {
   var client = new Faye.Client('/faye', {timeout: 120});
   
   client.subscribe('/game/updates', function(msg) {
+    if (!$("#updates ul").length) {
+      $("#updates").html("<ul>");
+    }
+    
+    var $li = $('<li style="display:none"><strong>' + msg.date + ":</strong> " + msg.update + "</li>");
+    
+    $("#updates ul").prepend($li);
+    $li.slideDown();
+    
     console.log("game update", msg);
   });
 });
